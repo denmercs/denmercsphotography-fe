654ba19ea2b80352d1728ffb6783b19602df72c8
denmercs <denmercs@gmail.com>
2020-02-26 21:53:09 -0600

    searching for a good library to use

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const home = () => {
+  return (
+    <div>
+      <h1>Denmercs Photography</h1>
+      <p>Making Matchless Memories</p>
+    </div>
+  );
+};
+
+export default home;
