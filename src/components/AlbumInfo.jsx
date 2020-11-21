1a69b509ebcb84d79761f144e1091e0457084c34
denmercs <denmercs@gmail.com>
2020-11-21 02:19:40 -0600

    fix loader styling in albuminfo component

--- src/components/AlbumInfo.jsx
+++ src/components/AlbumInfo.jsx
@@ -1,23 +1,30 @@
 import React, { useEffect } from 'react'
 import Gallery from 'react-grid-gallery';
+import {Container, Col, Row, Image} from 'react-bootstrap'
 import { useSelector, useDispatch } from 'react-redux'
 import { getPhotosById } from '../store/actions/facebookActions';
+import loadingGIF from "../assets/wedding.gif"
 import "./albumInfo.scss"
 
 
 const AlbumInfo = (props) => {
     const id = props.match.params.id;
     const dispatch = useDispatch();
-    const {photos} = useSelector(state => state.facebook)
+    const {photos, loading} = useSelector(state => state.facebook)
 
     useEffect(() => {
         dispatch(getPhotosById(id))
     }, [])
 
     return (
-        <div>
-            <Gallery images={photos} />
-        </div>
+        <Container>
+            <Col>
+                {
+                    (loading) ? (<Row className="loadingGIF"><Image src={loadingGIF}/></Row>) : <Gallery images={photos} />
+                    
+                }
+            </Col>
+        </Container>
     )
 }
 
